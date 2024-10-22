133ed43d32560854d29ea508d84c787e58afe4ab
Djuliatama <dwina147@gmail.com>
2024-10-22 22:08:32 +0700

    fiture :getAllBooks, getById, updateById

--- src/book.controller.js
+++ src/book.controller.js
@@ -1,3 +1,4 @@
+const { read } = require('fs');
 const { nanoid } = require('nanoid');         
 
 let books = []; 
@@ -51,7 +52,102 @@ const createBook = (req, res) => {
     });
 };
 
+const allBooks = [
+    {
+        id: "4j21vYIP22jF6s7MvbIpb",
+        name: "New Boook",
+        publisher: "Publisher Name"
+    },
+    {
+        id: "1L7ZtDUFeGs7VlEt",
+        name: "Buku B",
+        publisher: "Dicoding Indonesia"
+    },
+    {
+        id: "K8DZbfI-t3LrY7lD",
+        name: "Buku C",
+        publisher: "Dicoding Indonesia"
+    }
+ ];
+
+const getAllBooks = (req, res) => {
+    return res.status(200).json({
+        status: "success",
+        data: {
+            books: allBooks
+        }
+    });
+}
+
+const getById = (req, res) => {
+    const { bookId } = req.params;
+    const book = allBooks.find(book => book.id === bookId);
+
+    if (!book) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Buku tidak ditemukan"
+        });
+    }
+
+    return res.status(200).json({
+        status: "success",
+        data: {
+            book: book
+        }
+    });
+}
+
+const updateById = (req, res) => {
+    const { bookId } = req.params;
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = req.body;
+
+    if(!name) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Gagal memperbarui buku. Mohon isi nama buku"
+        });
+    }
+
+    if (readPage > pageCount) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+        });
+    }
+    
+    const bookExist = allBooks.find(book => book.id === bookId);
+    if (!bookExist) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Gagal memperbarui buku. Id tidak ditemukan"
+        });
+    }
+
+    allBooks = allBooks.map(book => 
+        book.id === bookId ? {
+            ...book, 
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+            updatedAt: new Date().toISOString()
+        } : book
+    );
+
+    return res.status(200).json({
+        status: "success",
+        message: "Buku berhasil diperbarui"
+    });
+};
 
 module.exports = {
     createBook,
+    getAllBooks,
+    getById,
+    updateById,
 }
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ const bookController = require ('./book.controller');
 router.post('/books', bookController.createBook);
 
 // // GET-get all books
-// router.get('/books', bookController.getAllBooks);
+router.get('/books', bookController.getAllBooks);
 
 // // GET-get a spesific book by ID 
-// router.get('/books/:bookId', bookController.getById);
+router.get('/books/:bookId', bookController.getById);
 
 // // PUT-update book by ID
-// router.put('/books/:bookId' , bookController.updateById);
+router.put('/books/:bookId' , bookController.updateById);
 
 // // DELETE-delete book by ID
 // router.delete('/books/:bookId', bookController.deleteById);
